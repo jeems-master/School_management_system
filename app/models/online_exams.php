a1f4395a038c8512834e2ef3fcd150fd152208ea
m4lien <silver@devroom.com>
2018-09-19 12:56:44 +0700

    add the relationship between online_exams and online_exams_passages

--- app/models/online_exams.php
+++ app/models/online_exams.php
@@ -2,4 +2,8 @@
 class online_exams extends Eloquent {
 	public $timestamps = false;
 	protected $table = 'online_exams';
+
+    public function passages(){
+        return $this->hasMany('online_exams_passages', 'examId', 'id');
+    }
 }
